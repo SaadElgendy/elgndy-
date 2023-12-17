5961264897178882dc955b10b5b41f29d657e934
SaadElgendy <138272532+SaadElgendy@users.noreply.github.com>
2023-12-17 04:46:48 +0200

    Add files via upload

---
+++ bills.js
@@ -0,0 +1,16 @@
+const print = document.querySelector('.invoice-print');
+const media = window.matchMedia('print');
+
+const update = (e) => print.style.display = e.matches ? 'none' : 'block';
+
+function convert() {
+  media.addEventListener('change',update,false);
+  window.print();
+}
+
+print.addEventListener('click',convert,false);
+
+
+
+
+console.log
